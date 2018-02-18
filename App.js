6751305cf2758d6dfd036cc867ef1e3ce08e6776
moirahuang <huang.moira@gmail.com>
2018-02-18 09:48:11 -0800

    kinda front end

--- App.js
+++ App.js
@@ -21,36 +21,37 @@ import {
   ImageBackground,
   Button
 } from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
 
-export default class CameraExample extends React.Component {
-
+export default class App extends React.Component {
+  render() {
+    return <RootStack / > ;
+  }
+}
+class HomeScreen extends React.Component {
   async componentDidMount() {
     await Font.loadAsync({
       'Bungee-Regular': require('./assets/Bungee-Regular.ttf'),
     });
     await Image.loadAsync({
-      'frontimage': require('./assets/title.png'),
+      'pieceoftrash': require('./assets/title.png'),
     });
     this.setState({
-      loading: false
+      textloading: false,
+      imageloading: false
     })
   }
   state = {
     image: null,
-    loading: true,
+    textloading: true,
+    imageloading: true,
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
   }
-  async componentWillMount() {
-    const {
-      status
-    } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({
-      hasCameraPermission: status === 'granted'
-    });
-  }
   render() {
-
+    if (this.state.imageloading) {
       return ( <
         ImageBackground source = {
           require('./assets/title.png')
@@ -77,213 +78,136 @@ export default class CameraExample extends React.Component {
             paddingVertical: 24
           }
         } >
-
         <
-        CustomButton onPress = {
-          () => {}
-        } > Press Anywhere To Continue < /CustomButton>  < /
-        View > <
-        /ImageBackground>
-      );
-    
-  }
-}
-  renderTakeImage = () => {
-    let {
-      image
-    } = this.state;
-    const {
-      hasCameraPermission
-    } = this.state;
-    if (this.state.loading || hasCameraPermission === null) {
-      return <View / > ;
-    } else if (hasCameraPermission === false) {
-      return <Text > No access to camera < /Text>;
-    } else {
-      return ( <
-          View style = {
-            {
-              flex: 1
-            }
-          } >
-          <
-          Camera style = {
-            {
-              flex: 1
-            }
-          }
-          ref = {
-            ref => (this.camera = ref)
-          }
-          type = {
-            this.state.type
-          } >
-
-          <
-          View style = {
-            {
-              flexDirection: 'row',
-              justifyContent: 'space-around',
-              alignItems: 'center',
-              position: 'absolute',
-              bottom: 0,
-              left: 0,
-              right: 0,
-              paddingVertical: 24
-            }
-          } >
-          <
-          CustomButton onPress = {
-            this._pickImage
-          } > Take Picture < /CustomButton> < /
-          View > <
-          /Camera> {
-          this.renderImage()
-        } <
-        /View>
-    );
-  }
-}
-renderImage = () => {
-  if (this.state.image != null) {
-    return ( <
-      ImageBackground source = {
-        {
-          uri: this.state.image
-        }
+        CustomButton  title = "Click To Continue" onPress = {
+            () => this.props.navigation.navigate('Details')
+          } > Click To Continue < /CustomButton>
+          </View >
+        <
+        /ImageBackground>);
       }
-      style = {
-        {
-          ...StyleSheet.absoluteFillObject,
-          backgroundColor: 'white',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }
-      } >
+    }
+  };
+  class DetailsScreen extends React.Component {
+    async componentDidMount() {
+   await Font.loadAsync({
+     'Bungee-Regular': require('./assets/Bungee-Regular.ttf'),
+   });
+   this.setState({loading: false})
+ }
+ state = {
+   image: null,
+   loading: true,
+   hasCameraPermission: null,
+   type: Camera.Constants.Type.back,
+ };
+ async componentWillMount() {
+   const { status } = await Permissions.askAsync(Permissions.CAMERA);
+   this.setState({ hasCameraPermission: status === 'granted' });
+ }
 
-      <
-      View style = {
-        {
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          right: 0,
-          paddingVertical: 34
-        }
-      } >
+ get footer() {
+   return (
+     <View
+       style={{
+         position: 'absolute',
+         bottom: 0,
+         left: 0,
+         right: 0,
+         backgroundColor: 'rgba(0,0,0,0.5)',
+         flexDirection: 'row',
+       }}/>
+   );
+ }
 
-      <
-      CustomButton onPress = {
-        () => {}
-      } > Recycle < /CustomButton> <
-      CustomButton onPress = {
-        () => {}
-      } > Trash < /CustomButton> <
-      CustomButton onPress = {
-        () => {}
-      } > Compost < /CustomButton> <
-      /View> <
-      /ImageBackground>
-    );
-  }
-};
-_pickImage = async () => {
-  const {
-    base64: data,
-    width,
-    height
-  } = await this.camera.takePictureAsync({
-    base64: true,
-  });
-  this.setState({
-    width,
-    height,
-    image: 'data:image/jpg;base64,' + data
-  });
+ render() {
+   let { image } = this.state;
+   const { hasCameraPermission } = this.state;
+   if (this.state.loading || hasCameraPermission === null) {
+     return <View />;
+   } else if (hasCameraPermission === false) {
+     return <Text>No access to camera</Text>;
+   } else {
+     return (
+       <View style={{ flex: 1 }}>
+         <Camera
+           style={{ flex: 1 }}
+           ref={ref => (this.camera = ref)}
+           type={this.state.type}>
+
+           <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', position: 'absolute', bottom: 0, left: 0, right: 0, paddingVertical: 24}}>
+               <CustomButton onPress={this._pickImage}>Take Picture</CustomButton>
+           </View>
+         </Camera>
+         {this.renderImage()}
+       </View>
+     );
+   }
+ }
+ renderImage = () => {
+   if (this.state.image != null) {
+     return (
+       <ImageBackground
+         source={{ uri: this.state.image }}
+         style={{
+           ...StyleSheet.absoluteFillObject,
+           backgroundColor: 'white',
+           alignItems: 'center',
+           justifyContent: 'center',
+         }}>
+
+       <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', position: 'absolute', bottom: 0, left: 0, right: 0, paddingVertical: 24}}>
+
+<CustomButton onPress={() => {   }}>Recycle</CustomButton>
+<CustomButton onPress={() => {   }}>Trash</CustomButton>
+         <CustomButton onPress={() => {   }}>Compost</CustomButton>
+         </View>
+       </ImageBackground>
+     );
+   }
+ };
+ _pickImage = async () => {
+   const { base64: data, width, height } = await this.camera.takePictureAsync({
+     base64: true,
+   });
+   this.setState({ width, height, image: 'data:image/jpg;base64,' + data });
+ };
 }
-
-/*
-<TouchableOpacity
-style={{
-  flex: 0.1,
-}}
-onPress={console.log("nanas")}>
-<Text
-style={{
-  textAlign: 'center',
-  color: 'white',
-  left: -122,
-  top: 313,
-  fontSize: 18,
-  fontFamily: 'Bungee-Regular',
-}}>
-  {' '}Recycle{' '}
-</Text>
-</TouchableOpacity>
-<TouchableOpacity
-style={{
-  flex: 0.1,
-}}
-onPress={console.log("nanas")}>
-<Text
-style={{
-  textAlign: 'center',
-  color: 'white',
-  left: -3,
-  top: 247,
-  fontSize: 18,
-  fontFamily: 'Bungee-Regular',
-}}>
-  {' '}Trash{' '}
-</Text>
-</TouchableOpacity>
-<TouchableOpacity
-style={{
-  flex: 0.1,
-}}
-onPress={console.log("nanas")}>
-<Text
-style={{
-  textAlign: 'center',
-  color: 'white',
-  left: 112,
-  top: 180,
-  fontSize: 18,
-  fontFamily: 'Bungee-Regular',
-}}>
-  {' '}Compost{' '}
-</Text>
-</TouchableOpacity>
-*/
-
+const RootStack = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Details: {
+    screen: DetailsScreen,
+  },
+}, {
+  initialRouteName: 'Home',
+});
 const CustomButton = ({
-onPress,
-style,
-children
-}) => ( <
-TouchableOpacity style = {
-  [{
-    backgroundColor: 'red'
-  }, style]
-}
-onPress = {
-  onPress
-} >
-<
-Text style = {
-  {
-    textAlign: 'center',
-    color: 'white',
-    fontSize: 22,
-    fontFamily: 'Bungee-Regular',
-    margin: 6
-  }
-} > {
+  onPress,
+  style,
   children
-} <
-/Text> <
-/TouchableOpacity>
+}) => ( <
+  TouchableOpacity style = {
+    [{
+      backgroundColor: 'red'
+    }, style]
+  }
+  onPress = {
+    onPress
+  } >
+  <
+  Text style = {
+    {
+      textAlign: 'center',
+      color: 'white',
+      fontSize: 22,
+      fontFamily: 'Bungee-Regular',
+      margin: 6
+    }
+  } > {
+    children
+  } <
+  /Text> < /
+  TouchableOpacity >
 )
