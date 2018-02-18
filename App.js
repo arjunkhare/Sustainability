bb84196565b927bb2fc266b576ec26e05922dd3f
moirahuang <huang.moira@gmail.com>
2018-02-17 22:58:02 -0800

    right after buttons started working

--- App.js
+++ App.js
@@ -13,13 +13,15 @@ import { Image, ImageBackground, Button } from 'react-native';
 
 export default class CameraExample extends React.Component {
 
-  componentDidMount() {
-    Font.loadAsync({
+  async componentDidMount() {
+    await Font.loadAsync({
       'Bungee-Regular': require('./assets/Bungee-Regular.ttf'),
     });
+    this.setState({loading: false})
   }
   state = {
     image: null,
+    loading: true,
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
   };
@@ -27,7 +29,7 @@ export default class CameraExample extends React.Component {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
   }
-  
+
   get footer() {
     return (
       <View
@@ -41,11 +43,23 @@ export default class CameraExample extends React.Component {
         }}/>
     );
   }
-  
+
   render() {
-    let { image } = this.state;
+    <ImageBackground
+      source={'.assets/title.png'}
+      style={{
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', position: 'absolute', bottom: 0, left: 0, right: 0, paddingVertical: 34}}>
+          <CustomButton onPress={this.renderTakeImage()}>Click Anywhere To Continue</CustomButton>
+      </View>
+  }
+      renderTakeImage = () => {
     const { hasCameraPermission } = this.state;
-    if (hasCameraPermission === null) {
+    if (this.state.loading || hasCameraPermission === null) {
       return <View />;
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
@@ -56,33 +70,9 @@ export default class CameraExample extends React.Component {
             style={{ flex: 1 }}
             ref={ref => (this.camera = ref)}
             type={this.state.type}>
-            
-            <View     
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
 
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignItems: 'right',
-                }}
-                onPress={this._pickImage}>
-                <Text
-                  style={{
-                    fontSize: 18,
-                    marginTop: 500,
-                    marginLeft: 100,
-                    marginBottom: 100,
-                    fontFamily: 'Bungee-Regular',
-                    color: 'white',
-                  }}>
-                  {' '}Take Picture{' '}
-                </Text>
-              </TouchableOpacity>
-
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', position: 'absolute', bottom: 0, left: 0, right: 0, paddingVertical: 34}}>
+                <CustomButton onPress={this._pickImage}>Take Picture</CustomButton>
             </View>
           </Camera>
           {this.renderImage()}
@@ -90,73 +80,24 @@ export default class CameraExample extends React.Component {
       );
     }
   }
-
   renderImage = () => {
     if (this.state.image != null) {
       return (
         <ImageBackground
           source={{ uri: this.state.image }}
           style={{
-            resizeMode: 'contain',
             ...StyleSheet.absoluteFillObject,
             backgroundColor: 'white',
             alignItems: 'center',
             justifyContent: 'center',
           }}>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignItems: 'center',
-            }}
-            onPress={console.log("nanas")}>
-            <Text
-              style={{
-                textAlign: 'center',
-                color: 'white',
-                position: 'absolute',
-                bottom: 100,
-                fontSize: 18,
-                fontFamily: 'Bungee-Regular',
-              }}>
-              {' '}Recycle{' '}
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignItems: 'center',
-            }}
-            onPress={console.log("nanas")}>
-            <Text
-              style={{
-                textAlign: 'center',
-                color: 'white',
-                position: 'absolute',
-                bottom: 100,
-                fontSize: 18,
-                fontFamily: 'Bungee-Regular',
-              }}>
-              {' '}Trash{' '}
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignItems: 'center',
-            }}
-            onPress={console.log("nanas")}>
-            <Text
-              style={{
-                textAlign: 'center',
-                color: 'white',
-                position: 'absolute',
-                bottom: 100,
-                fontSize: 18,
-                fontFamily: 'Bungee-Regular',
-              }}>
-              {' '}Compost{' '}
-            </Text>
-          </TouchableOpacity>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', position: 'absolute', bottom: 0, left: 0, right: 0, paddingVertical: 34}}>
+
+<CustomButton onPress={() => {   }}>Recycle</CustomButton>
+<CustomButton onPress={() => {   }}>Trash</CustomButton>
+          <CustomButton onPress={() => {   }}>Compost</CustomButton>
+          </View>
         </ImageBackground>
       );
     }
@@ -168,3 +109,76 @@ export default class CameraExample extends React.Component {
     this.setState({ width, height, image: 'data:image/jpg;base64,' + data });
   };
 }
+
+/*
+<TouchableOpacity
+  style={{
+    flex: 0.1,
+  }}
+  onPress={console.log("nanas")}>
+  <Text
+  style={{
+    textAlign: 'center',
+    color: 'white',
+    left: -122,
+    top: 313,
+    fontSize: 18,
+    fontFamily: 'Bungee-Regular',
+  }}>
+    {' '}Recycle{' '}
+  </Text>
+</TouchableOpacity>
+<TouchableOpacity
+  style={{
+    flex: 0.1,
+  }}
+  onPress={console.log("nanas")}>
+  <Text
+  style={{
+    textAlign: 'center',
+    color: 'white',
+    left: -3,
+    top: 247,
+    fontSize: 18,
+    fontFamily: 'Bungee-Regular',
+  }}>
+    {' '}Trash{' '}
+  </Text>
+</TouchableOpacity>
+<TouchableOpacity
+  style={{
+    flex: 0.1,
+  }}
+  onPress={console.log("nanas")}>
+  <Text
+  style={{
+    textAlign: 'center',
+    color: 'white',
+    left: 112,
+    top: 180,
+    fontSize: 18,
+    fontFamily: 'Bungee-Regular',
+  }}>
+    {' '}Compost{' '}
+  </Text>
+</TouchableOpacity>
+*/
+
+const CustomButton = ({onPress, style, children}) => (
+  <TouchableOpacity
+    style={[{
+      backgroundColor: 'red'
+    }, style]}
+    onPress={onPress}>
+    <Text
+    style={{
+      textAlign: 'center',
+      color: 'white',
+      fontSize: 22,
+      fontFamily: 'Bungee-Regular',
+      margin: 6
+    }}>
+      {children}
+    </Text>
+  </TouchableOpacity>
+)
